4644d831ea21309ebe6d78d10d2d7bd2e0258809
mayra@mumuki.org <mayra@mumuki.org>
2021-12-20 13:33:18 +0000

    Mumuki Export on 2021-12-20 13:33:18 +0000

--- 00004_Su superclase/test.py
+++ 00004_Su superclase/test.py
@@ -58,7 +58,10 @@
     dispositivo = Dispositivo(1)
     self.assertTrue("cargar_a_tope" in dir(dispositivo) and callable(dispositivo.cargar_a_tope))
     
-  def test_el_método_cargar_a_tope_no_esta_definido_en_la_clase_Celular(self):
+  def test_el_método_utilizar_está_definido_en_la_clase_Celular(self):
     celular = Celular(1)
-    self.assertFalse("cargar_a_tope" in dir(celular) and callable(celular.cargar_a_tope))
+    self.assertTrue("utilizar" in dir(celular) and callable(celular.utilizar))
     
+  def test_el_método_cargar_a_tope_no_está_definido_en_la_clase_Celular(self):
+    celular = Celular(1)
+    self.assertTrue("cargar_a_tope" in dir(celular) and callable(celular.cargar_a_tope))
