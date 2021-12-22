7c460d654618b00767440be09dd67e660089440f
mayra@mumuki.org <mayra@mumuki.org>
2021-12-22 14:28:19 +0000

    Mumuki Export on 2021-12-22 14:28:19 +0000

--- 00001_Aflojá con el aparatito/default.py
+++ 00001_Aflojá con el aparatito/default.py
@@ -1,10 +1,18 @@
 class Celular:
   
-  def __init__(self, bateria):
+  def __init__(self, bateria, saldo, sistema_operativo):
     self.bateria = bateria
+    self.saldo = saldo
+    self.sistema_operativo = sistema_operativo
 
   def tiene_bateria(self):
     return self.bateria > 20
 
   def tiene_bateria_maxima(self):
     return self.bateria == 100
+
+  def necesita_saldo(self):
+    return self.saldo == 0
+    
+  def cargar_saldo(self, saldo):
+    return self.saldo += saldo
