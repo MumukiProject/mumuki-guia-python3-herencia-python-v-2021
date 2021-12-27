a316fbb8e2c0e90eacc10e16ff4bdc58c831ea80
mayra@mumuki.org <mayra@mumuki.org>
2021-12-27 14:45:05 +0000

    Mumuki Export on 2021-12-27 14:45:05 +0000

--- 00001_Aflojá con el aparatito/test.py
+++ 00001_Aflojá con el aparatito/test.py
@@ -17,7 +17,19 @@
     self.assertEqual(tablet.bateria,75)
 
   def test_Una_tablet_vuelve_a_tener_100_de_bateria_si_la_cargo_a_tope(self):
-    tablet = Tablet(100)
-    tablet.utilizar(200)
+    tablet = Tablet(10)
     tablet.cargar_a_tope()
     self.assertEqual(tablet.bateria,100)
+    
+  def test_Una_tablet_tiene_bateria_maxima_si_tiene_100_de_bateria(self):
+    tablet = Tablet(100)
+    tablet.cargar_a_tope()
+    self.assertTrue(tablet.tiene_bateria_maxima())
+    
+  def test_una_notebook_tiene_bateria_si_tiene_más_de_20_de_bateria(self):
+    tablet = Tablet(21)
+    self.assertTrue(tablet.tiene_bateria)  
+    
+  def test_una_notebook_no_tiene_bateria_si_tiene_20_de_bateria(self):
+    tablet = Tablet(20)
+    self.assertTrue(tablet.tiene_bateria)  
