8c749a647e48b243474e5de22944d1eb88b655f0
gustavo@mumuki.org <gustavo@mumuki.org>
2022-01-05 21:03:03 +0000

    Mumuki Export on 2022-01-05 21:03:03 +0000

--- 00011_Es un trabajo para super/test.py
+++ 00011_Es un trabajo para super/test.py
@@ -6,15 +6,10 @@
     
   def test_un_colectivo_pierde_sus_pasajeros_al_cargar_combustible(self):
     colectivo = Colectivo()
-    colectivo.cargar_pasajeros(100)
+    colectivo.pasajeros = 100
     colectivo.cargar_combustible(50)
     self.assertEqual(colectivo.pasajeros,0)
   
   def test_la_clase_Colectivo_define_el_método_cargar_combustible(self):
     colectivo = Colectivo()
     self.assertTrue("cargar_combustible" in dir(colectivo) and callable(colectivo.cargar_combustible))
-    
-    
-# Seguro hay alguna manera de definir cargar_pasajeros en el extra code, pero no pude hacerlo, habría que evaluar si se lo pedimos o lo agregamos
-#def cargar_pasajeros(self,cant_pasajeros):
-#    self.pasajeros += cant_pasajeros
