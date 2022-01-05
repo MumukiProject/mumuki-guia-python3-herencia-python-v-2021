9e8eea76b33c4cb915959b52d63b55b825f86904
mayra@mumuki.org <mayra@mumuki.org>
2022-01-05 18:38:49 +0000

    Mumuki Export on 2022-01-05 18:38:49 +0000

--- 00013_Herencia zombi/default.py
+++ 00013_Herencia zombi/default.py
@@ -1,41 +1,29 @@
 class Zombi:
-  def __init__(self,salud_inicial):
-    self.salud = salud_inicial
-  
-  def salud(self):
-    return self.salud
-  
-  def gritar(self):
-    return "¡agrrrg!"
-  
-  def sabe_correr(self):
-    return False
-  
-  def sin_vida(self):
-    return self.salud == 0
-  
-  def recibir_danio(self,puntos):
-    self.salud = max(self.salud - puntos * 2, 0)
-
-
-class SuperZombi:
-  def __init__(self,salud_inicial):
-    self.salud = salud_inicial
-
-  def salud(self):
-    return self.salud
-
-  def gritar(self):
-    return "¡agrrrg!"
+  def __init__(self, un_hambre):
+    self.hambre = un_hambre
 
   def sabe_correr(self):
     return True
 
-  def sin_vida(self):
-    return self.salud == 0
+  def es_un_peligro(self):
+    return self.hambre > 50
 
-  def recibir_danio(self,puntos):
-    self.salud = max(self.salud - puntos * 3, 0)
-  
+  def recibir_danio(self,puntos_de_danio):
+    self.hambre -= puntos_de_danio * 2
+
+class SuperZombi:
+  def __init__(self,un_hambre):
+    self.hambre = un_hambre
+
+  def sabe_correr(self):
+    return True
+
+  def es_un_peligro(self):
+    return True
+
+  def recibir_danio(self,puntos_de_danio):
+    self.hambre -= puntos_de_danio
+
   def regenerarse(self):
-    self.salud = 100
+    self.hambre = 100
+
