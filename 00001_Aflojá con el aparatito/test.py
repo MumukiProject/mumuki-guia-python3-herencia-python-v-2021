b2247321ba75ece0cdc1c43b473c9dfee57d0bc0
gustavo@mumuki.org <gustavo@mumuki.org>
2022-01-05 20:37:56 +0000

    Mumuki Export on 2022-01-05 20:37:56 +0000

--- 00001_Aflojá con el aparatito/test.py
+++ 00001_Aflojá con el aparatito/test.py
@@ -4,12 +4,10 @@
     tablet.utilizar(60)
     self.assertEqual(tablet.bateria,70)
     
-    
   def test_Una_tablet_gasta_15_de_bateria_si_la_uso_media_hora(self):
     tablet = Tablet(100)
     tablet.utilizar(30)
     self.assertEqual(tablet.bateria,85)
-    
     
   def test_Una_tablet_gasta_25_de_bateria_si_la_uso_50_minutos(self):
     tablet = Tablet(100)
@@ -23,8 +21,11 @@
     
   def test_Una_tablet_tiene_bateria_maxima_si_tiene_100_de_bateria(self):
     tablet = Tablet(100)
-    tablet.cargar_a_tope()
     self.assertTrue(tablet.tiene_bateria_maxima)
+    
+  def test_Una_tablet_no_tiene_bateria_maxima_si_tiene_menos_de_100_de_bateria(self):
+    tablet = Tablet(99)
+    self.assertFalse(tablet.tiene_bateria_maxima)
     
   def test_una_notebook_tiene_bateria_si_tiene_más_de_20_de_bateria(self):
     tablet = Tablet(21)
@@ -32,4 +33,8 @@
     
   def test_una_notebook_no_tiene_bateria_si_tiene_20_de_bateria(self):
     tablet = Tablet(20)
-    self.assertTrue(tablet.tiene_bateria)  
+    self.assertFalse(tablet.tiene_bateria) 
+    
+  def test_una_notebook_no_tiene_bateria_si_tiene_menos_de_20_de_bateria(self):
+    tablet = Tablet(19)
+    self.assertFalse(tablet.tiene_bateria)
