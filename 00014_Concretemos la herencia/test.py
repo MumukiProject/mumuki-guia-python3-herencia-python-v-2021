4d977c429e9413016d283529d99a932b76574f73
mayra@mumuki.org <mayra@mumuki.org>
2022-01-05 18:49:07 +0000

    Mumuki Export on 2022-01-05 18:49:06 +0000

--- 00014_Concretemos la herencia/test.py
+++ 00014_Concretemos la herencia/test.py
@@ -4,12 +4,12 @@
     self.assertTrue("descansar" in dir(zombi) and callable(zombi.descansar))
     
     
-  def test_Cuando_un_Zombi_descansa_una_cantidad_de_minutos_restaura_su_salud_en_esa_cantidad(self):
+  def test_Cuando_un_Zombi_descansa_una_cantidad_de_minutos_restaura_su_hambre_en_esa_cantidad(self):
     zombi = Zombi(44)
     zombi.descansar(56)
-    self.assertEqual(zombi.salud, 100) 
+    self.assertEqual(zombi.hambre, 100) 
     
-  def test_Cuando_un_SuperZombi_descansa_una_cantidad_de_minutos_restaura_su_salud_en_esa_cantidad(self):
+  def test_Cuando_un_SuperZombi_descansa_una_cantidad_de_minutos_restaura_su_hambre_en_esa_cantidad(self):
     zombi = SuperZombi(44)
     zombi.descansar(56)
-    self.assertEqual(zombi.salud, 100) 
+    self.assertEqual(zombi.hambre, 100) 
